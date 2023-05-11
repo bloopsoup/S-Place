b2ad1968b2a763a5cecd7c0f686c4035eb6a061c
Leo Chung <leochungus@berkeley.edu>
2023-05-10 21:23:34 -0700

    added action map

--- boggersJS/components/index.js
+++ boggersJS/components/index.js
@@ -1,6 +1,7 @@
 /** Components used to construct complex GameObjects.
  *  @namespace Components */
 export * from './movable/index.js';
+export { default as ActionMap } from './action-map.js';
 export { default as Collider } from './collider.js';
 export { default as CollisionMap } from './collision-map.js'; 
 export { default as Health } from './health.js';
