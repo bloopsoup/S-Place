6b70c3fe4cd7b16a37971fb8f9040a6e9e4b1e30
Leo Chung <leochungus@berkeley.edu>
2023-06-22 12:02:51 -0700

    added label skeleton

--- boggersJS/components/index.js
+++ boggersJS/components/index.js
@@ -4,5 +4,6 @@ export * from './movable/index.js';
 export { default as Collider } from './collider.js';
 export { default as CollisionMap } from './collision-map.js'; 
 export { default as Health } from './health.js';
+export { default as Label } from './label.js';
 export { default as Sprite } from './sprite.js';
 export { default as TickRunner } from './tick-runner.js';
---
+++ boggersJS/components/label.js
@@ -0,0 +1,7 @@
+/** The Label handles displaying text. It uses a separate Canvas internally to render text.
+ *  @memberof Components */
+class Label {
+
+}
+
+export default Label;
