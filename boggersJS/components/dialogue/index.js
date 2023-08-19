4627a1c75cdea4257540e4b069b051d5dc3e701a
Leo Chung <leochungus@berkeley.edu>
2023-08-19 15:23:11 -0700

    expanded dialogue index

--- boggersJS/components/dialogue/index.js
+++ boggersJS/components/dialogue/index.js
@@ -1,3 +1,6 @@
 /** Components used to handle dialogue trees.
  *  @namespace Components.Dialogue */
+export { default as DialogueNode } from './dialogue-node.js';
 export { default as Dialogue } from './dialogue.js';
+export { default as DScriptParser } from './dscript-parser.js';
+export { default as DScriptReader } from './dscript-reader.js';
