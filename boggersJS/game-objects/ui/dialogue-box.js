5f38ab690b7c9b4df97d0c6f92f77a4b29aa2bba
Leo Chung <leochungus@berkeley.edu>
2023-09-26 00:09:03 -0700

    draft dbox

--- boggersJS/game-objects/ui/dialogue-box.js
+++ boggersJS/game-objects/ui/dialogue-box.js
@@ -1,63 +1,52 @@
 import GameObject from '../game-object.js';
-import { Movable, Collider, Sprite } from '../../components/index.js';
+import { Movable, Collider, Sprite, Dialogue, DScriptReader, DScriptParser, Label } from '../../components/index.js';
 import { InputTracker, Vector2 } from '../../common/index.js';
 
+/** A textbox which displays dialogue.
+ *  @augments GameObject
+ *  @memberof GameObjects.Display */
 class DialogueBox extends GameObject {
-    constructor(sprite, pos, msgArray) {
+    /** @type {Dialogue} */
+    #dialogue
+    /** @type {Label} */
+    #label
+
+    /** Create the dialogue box.
+     *  @param {Sprite} sprite - The sprite of the dialogue box.
+     *  @param {Vector2} pos - The position of the dialogue box. 
+     *  @param {string} font - The font of the displayed dialogue. 
+     *  @param {string} script - The dialogue script in DScript format. */
+    constructor(sprite, pos, font, script) {
         super();
         this.sprite = sprite;
-        this.pos = pos;
-        this.msgArray = msgArray;
-
         this.movable = new Movable(new Vector2(0, 0), this.sprite.dimensions, pos, new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0));
-
-        this.msgIndex = 0;
-        this.msg = this.msgArray[this.msgIndex];
-        this.counter = 0;
-        this.display = '';
-
-        this.doneTyping = false;
-        this.msgDone = false;
+        this.collider = new Collider(0);
+        this.#dialogue = new Dialogue(new DScriptParser(new DScriptReader(script).read()).parse());
+        this.#label = new Label('', this.sprite.dimensions, new Vector2(10, 10), font);
     }
 
-    handleInputs(inputs) {
-        if (! this.msgDone && this.doneTyping && inputs.has('MouseHold')) {
-            if (this.collider.pointOverlaps(this.movable, inputs.get('MouseHold').pos)) {
-                this.msgIndex ++;
-                this.msg = this.msgArray[this.msgIndex];
-                this.display = '';
-                this.doneTyping = false;
-            }
-        } 
-    }
-
-    update(dt) {
-        if (! this.doneTyping && ! this.msgDone) {
-            if (this.display.length == this.msg.length) {
-                this.doneTyping = true;
-                if (this.msgIndex == this.msgArray.length - 1) {this.msgDone = true;}
-            }
-            else if (this.counter == 0) {
-                const str = this.msg[this.display.length];
-                if (str === '.' || str === ',') {
-                    this.counter += 50;
-                }
-                else if (str === ' ') {
-                    this.counter += 20;
-                }
-                else {
-                    this.counter += 5;
-                }
-                this.display += str;
-            }
-            else {
-                this.counter --;
-            }
+    /** Handle inputs and update components.
+     *  @see GameObject.update
+     *  @param {InputTracker} inputs */
+    update(inputs) {
+        if (inputs.has('MouseHold') && this.collider.pointOverlaps(this.movable, inputs.get('MouseHold').pos)) {
+            this.#dialogue.advance();
+            this.#label.clear();
         }
+
+        this.#dialogue.updateText();
+        // TODO: instead of using ADD, implement a setter for the label's message
+        if (this.#dialogue.messageContent !== null) this.#label.add(this.#dialogue.messageContent[this.#dialogue.messageContent.length - 1]);
     }
-    
+
+    /** Draw the object.
+     *  @see GameObject.draw
+     *  @param {CanvasRenderingContext2D} context
+     *  @param {number} alpha */
     draw(context, alpha){
-        context.font = "48px serif";
-        context.fillText(this.display, this.movable.pos.x, this.movable.pos.y);
+        this.sprite.draw(context, this.movable.pos);
+        this.#label.draw(context, this.movable.pos);
     }
 }
+
+export default DialogueBox;
--- boggersJS/game-objects/ui/index.js
+++ boggersJS/game-objects/ui/index.js
@@ -1,4 +1,5 @@
 /** Display UI.
  *  @namespace GameObjects.UI */
 export { default as Button } from './button.js';
+export { default as DialogueBox } from './dialogue-box.js';
 export { default as TextInput } from './text-input.js';
