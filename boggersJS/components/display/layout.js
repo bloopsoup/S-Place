592e575aba86849b657eb0076bd726342812d367
Leo Chung <leochungus@berkeley.edu>
2023-10-24 22:55:01 -0700

    updated layout

--- boggersJS/components/display/layout.js
+++ boggersJS/components/display/layout.js
@@ -1,50 +1,34 @@
 import Label from './label.js';
+import LayoutSlot from './layout-slot.js';
 import Sprite from './sprite.js';
 import { Vector2 } from '../../common/index.js';
 
 /** An arrangement of slots that can fit Sprite and Label components.
- *  Provides auto-centering for placing sprites and auto-dimensions for
- *  placing labels. Should ONLY be used for creating more complex UI objects.
+ *  Should ONLY be used for creating more complex UI objects.
  *  @memberof Components.Display */
 class Layout {
-    /** @type {Object<string, Label>} */
-    #labels
-    /** @type {Object<string, Sprite>} */
-    #sprites
-    /** @type {Object<string, Vector2>} */
-    #offsets
+    /** @type {Object<string, LayoutSlot>} */
+    #slots
 
     /** Create the layout.
-     *  @param {Object<string, Label>} labels - A dictionary of labels.
-     *  @param {Object<string, Sprite>} sprites - A dictionary of sprites.
-     *  @param {Object<string, Vector2>} offsets - A mapping between names and offsets to use when placing 
-     *      each element relative to the layout. For names that don't have positions, the default offset 
-     *      will be (0, 0). If a label and sprite share the same name, they'll use the same offset. */
-    constructor(labels, sprites, offsets = {}) {
-        this.#labels = labels;
-        this.#sprites = sprites;
-        this.#offsets = {};
-        Object.keys(offsets).forEach(name => this.#offsets[name] = offsets[name]);
-        Object.keys(labels).forEach(name => { if (!(name in this.#offsets)) this.#offsets[name] = new Vector2(0, 0); });
-        Object.keys(sprites).forEach(name => { if (!(name in this.#offsets)) this.#offsets[name] = new Vector2(0, 0); });
-    }
+     *  @param {Object<string, LayoutSlot>} slots - A mapping between names and layout slots. */
+    constructor(slots) { this.#slots = slots; }
 
-    /** Gets a label from the layout. This is the primary way to change a label's text.
-     *  @param {string} name - The name of the label to retrieve. 
-     *  @returns {Label} The label. */
-    getLabel(name) { return this.#labels[name]; }
+    /** Adds a label to a layout slot.
+     *  @param {string} name - The name of the slot to add to. 
+     *  @param {Label} label - The label to add. */
+    addLabel(name, label) { this.#slots[name].label = label; }
 
-    /** Gets a sprite from the layout. This is the primary way to change frames.
-     * @param {string} name - The name of the sprite to retrieve.
-     * @returns {Sprite} The sprite. */
-    getSprite(name) { return this.#sprites[name]; }
+    /** Adds a sprite to a layout slot.
+     *  @param {string} name - The name of the slot to add to.
+     *  @param {Sprite} sprite - The sprite to add. */
+    addSprite(name, sprite) { this.#slots[name].sprite = sprite; }
 
     /** Draw the layout's elements.
      *  @param {CanvasRenderingContext2D} context - The context to draw on.
      *  @param {Vector2} pos - The anchor position that all elements will be drawn. */
     draw(context, pos) {
-        Object.keys(this.#labels).forEach(name => this.#labels[name].draw(context, pos.addCopy(this.#offsets[name])));
-        Object.keys(this.#sprites).forEach(name => this.#labels[name].draw(context, pos.addCopy(this.#offsets[name]))); 
+        Object.keys(this.#slots).forEach(name => this.#slots[name].draw(context, pos));
     }
 }
 
